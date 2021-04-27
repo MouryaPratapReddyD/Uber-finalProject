cb5ab103e1e44ff7aef4679c259426a844c2194d
Mourya Pratap Reddy <dasarapalli.m@northeastern.edu>
2021-04-27 15:05:37 -0400

    added .git in source location github url

--- CI_terraform/main.tf
+++ CI_terraform/main.tf
@@ -1,7 +1,6 @@
 # 
 # Resources: CodeBuild
 # 
-
 resource "aws_codebuild_project" "uberCI" {
   name          = "uberCI"
   description   = "Build and test uber docker image"
@@ -19,19 +18,19 @@ resource "aws_codebuild_project" "uberCI" {
     image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
     privileged_mode             = "true"
-
+    //image_pull_credentials_type = "SERVICE_ROLE"
     environment_variable {
       name  = "AWS_ACCOUNT_ID"
-      value = "211252803163"
+      value = "847450032545"
     }
     environment_variable {
       name  = "AWS_REGION"
-      value = "us-east-2"
+      value = "us-east-1"
     }
  
    source {
     type                = "GITHUB"
-    location            = "https://github.com/MouryaPratapReddyD/Uber-finalProject"
+    location            = "https://github.com/MouryaPratapReddyD/Uber-finalProject.git"
     git_clone_depth     = "1" 
     report_build_status = true
     auth {
@@ -46,5 +45,3 @@ resource "aws_codebuild_webhook" "uberCI" {
   project_name = aws_codebuild_project.uberCI.name
 
 }
-
-
